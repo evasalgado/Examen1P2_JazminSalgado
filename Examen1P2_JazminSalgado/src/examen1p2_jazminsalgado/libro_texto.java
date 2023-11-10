a198ae66c3d7acc3762f3727df1f31de1ad02c17
evasalgado <imviarsha@gmail.com>
2023-11-10 13:26:38 -0600

    clase libro de texto agregado

---
+++ Examen1P2_JazminSalgado/src/examen1p2_jazminsalgado/libro_texto.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen1p2_jazminsalgado;
+
+/**
+ *
+ * @author evaja
+ */
+public class libro_texto extends libro{
+    private String curso, facultad;
+
+    public libro_texto() {
+        super();
+    }
+
+    public libro_texto(String curso, String facultad, String titulo, String autor, double precio, String edicion) {
+        super(titulo, autor, precio, edicion);
+        this.curso = curso;
+        this.facultad = facultad;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    @Override
+    public String toString() {
+        return "curso=" + curso + "\nfacultad=" + facultad ;
+    }
+    
+    
+}
